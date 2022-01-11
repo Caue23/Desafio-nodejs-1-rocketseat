518b463b8b1a92f8cc4ce779055aa091b620d883
Caue23 <90137489+Caue23@users.noreply.github.com>
2022-01-11 11:59:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,127 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const{username} = request.headers;
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(400).json({error: "users not fund"});
+};
+
+  request.user = user;
+
+  return next();
+};
+
+function checksUsernameExists (resquest, response, next) {
+  const{username} = resquest.body;
+  const checks = users.find(user => user.username==username);
+
+  if(checks) {
+    return response.status(400).json({error: "Usuario existente"})
+  };
+  return next();
+};
+
+
+app.post ('/users', checksUsernameExists, (request, response) => {
+  const {name, username} =  request.body;
+  
+    users.push({ 
+      id: uuidv4 (),
+      name, 
+      username, 
+      todos: []
+    });
+   
+    const userExists = users.find(user => user.username === username);
+
+    return response.status(201).json(userExists);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+    
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const  { title, deadline} = request.body;
+  const { user } = request;
+  
+  
+  user.todos.push({
+      id: uuidv4(), 
+      title,
+      done: false, 
+      deadline: new Date(deadline), 
+      created_at:new Date()
+    
+  });
+  
+  const todosOperation = user.todos.find(todo => todo.title === title);
+  
+  return response.status(201).json(todosOperation);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+  
+  const todo = user.todos.find(todo => todo.id == id);
+
+  if (!todo) {
+    return response.status(404).json({error: "EveryoneOperation not fund"})
+  };
+
+  todo.title = title;
+  todo.deadline = new Date(deadline)
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({error: "EveryoneOperation not fund"})
+  };
+
+  
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const {id} = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+  
+  if (todoIndex === -1) {
+    return response.status(404).json({error: "EveryoneOperation not fund"})
+  };
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
